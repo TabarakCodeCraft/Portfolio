e981cb9e6365cc064ec5966dc0c424f3a3379e0a
TabarakCodeCraft <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2025-02-21 01:25:34 +0300

    first commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiGit,
+  DiCss3
+} from "react-icons/di";
+import {
+  SiFirebase,
+  SiNextdotjs,
+  SiPostgresql,
+} from "react-icons/si";
+import { TfiHtml5 } from "react-icons/tfi";
+import { SiTailwindcss } from "react-icons/si";
+import { AiOutlineConsoleSql } from "react-icons/ai";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col> 
+      <Col xs={4} md={2} className="tech-icons">
+        <TfiHtml5 />
+      </Col> 
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col> 
+        <Col xs={4} md={2} className="tech-icons">
+        <SiTailwindcss />
+      </Col> 
+    
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+     
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+    
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+      </Col> <Col xs={4} md={2} className="tech-icons">
+        <AiOutlineConsoleSql />
+      </Col>
+     
+      
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiFigma
+} from "react-icons/si";
+import { FaAws } from "react-icons/fa";
+
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaAws />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
