054f09e2944a5ce26e6d28f5e85585e6c4682e07
TabarakCodeCraft <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2025-02-21 18:44:33 +0300

    new update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/carlover.png";
 import img1 from "../../Assets/mycv.png";
 import img2 from "../../Assets/drlap.png";
