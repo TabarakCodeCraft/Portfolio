c10142c25a8e0103420307d6347481dce825e180
TabarakCodeCraft <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2025-02-22 22:08:27 +0300

    add name for tools

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Col, Row } from "react-bootstrap";
 import {
   DiJavascript1,
@@ -17,50 +17,47 @@ import { TfiHtml5 } from "react-icons/tfi";
 import { SiTailwindcss } from "react-icons/si";
 import { AiOutlineConsoleSql } from "react-icons/ai";
 
+const TechIcon = ({ icon: Icon, name }) => {
+  const [showName, setShowName] = useState(false);
+
+  return (
+    <Col 
+      xs={4} 
+      md={2} 
+      className="tech-icons relative"
+      onClick={() => setShowName(!showName)}
+    >
+      <Icon />
+      {showName && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-75 text-white text-lg">
+          {name}
+        </div>
+      )}
+    </Col>
+  );
+};
+
 function Techstack() {
+  const skills = [
+    { Icon: DiJavascript1, name: 'JavaScript' },
+    { Icon: TfiHtml5, name: 'HTML5' },
+    { Icon: DiCss3, name: 'CSS3' },
+    { Icon: SiTailwindcss, name: 'Tailwind CSS' },
+    { Icon: DiNodejs, name: 'Node.js' },
+    { Icon: DiReact, name: 'React' },
+    { Icon: SiNextdotjs, name: 'Next.js' },
+    { Icon: DiMongodb, name: 'MongoDB' },
+    { Icon: DiGit, name: 'Git' },
+    { Icon: SiFirebase, name: 'Firebase' },
+    { Icon: SiPostgresql, name: 'PostgreSQL' },
+    { Icon: AiOutlineConsoleSql, name: 'SQL' }
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col> 
-      <Col xs={4} md={2} className="tech-icons">
-        <TfiHtml5 />
-      </Col> 
-      <Col xs={4} md={2} className="tech-icons">
-        <DiCss3 />
-      </Col> 
-        <Col xs={4} md={2} className="tech-icons">
-        <SiTailwindcss />
-      </Col> 
-    
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-     
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-    
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col> <Col xs={4} md={2} className="tech-icons">
-        <AiOutlineConsoleSql />
-      </Col>
-     
-      
+      {skills.map((skill, index) => (
+        <TechIcon key={index} icon={skill.Icon} name={skill.name} />
+      ))}
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
@@ -9,26 +9,40 @@ import {
 } from "react-icons/si";
 import { FaAws } from "react-icons/fa";
 
+const ToolIcon = ({ icon: Icon, name }) => {
+  const [showName, setShowName] = useState(false);
+
+  return (
+    <Col 
+      xs={4} 
+      md={2} 
+      className="tech-icons relative"
+      onClick={() => setShowName(!showName)}
+    >
+      <Icon />
+      {showName && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-75 text-white text-lg">
+          {name}
+        </div>
+      )}
+    </Col>
+  );
+};
 
 function Toolstack() {
+  const tools = [
+    { Icon: SiVisualstudiocode, name: 'VS Code' },
+    { Icon: SiFigma, name: 'Figma' },
+    { Icon: SiPostman, name: 'Postman' },
+    { Icon: FaAws, name: 'AWS' },
+    { Icon: SiVercel, name: 'Vercel' }
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFigma />
-      </Col>
-
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaAws />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
+      {tools.map((tool, index) => (
+        <ToolIcon key={index} icon={tool.Icon} name={tool.name} />
+      ))}
     </Row>
   );
 }
